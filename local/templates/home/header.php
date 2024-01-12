6cc975fb994ac389e2e3d2027d82bc512c4d57dc
ZenArtZ98 <sergey.volkov98@yandex.ru>
2024-01-12 11:30:01 +0300

    task 156520

--- local/templates/home/header.php
+++ local/templates/home/header.php
@@ -26,9 +26,15 @@ IncludeTemplateLangFile(__FILE__);
     Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/js/mediaelement-and-player.min.js");
     Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/js/jquery.stellar.min.js");
     Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/js/jquery.countdown.min.js");
-    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "js/jquery.magnific-popup.min.js");
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/js/jquery.magnific-popup.min.js");
     Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/js/bootstrap-datepicker.min.js");
     Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/js/aos.js");
+
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/js/main.js");
+
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/js/player.js");
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/js/slick.min.js");
+
     Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/js/main.js");
     // для css-файлов
     Asset::getInstance()->addCss("https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,700,900|Roboto+Mono:300,400,500");
@@ -50,7 +56,9 @@ IncludeTemplateLangFile(__FILE__);
 </head>
 
 <body>
-<?$APPLICATION->ShowPanel();?>
+<div id="panel">
+    <? $APPLICATION->ShowPanel(); ?>
+</div>
   <div class="site-loader"></div>
 
   <div class="site-wrap">
@@ -69,16 +77,36 @@ IncludeTemplateLangFile(__FILE__);
         <div class="row align-items-center">
           <div class="col-6 col-md-6">
             <p class="mb-0">
-              <a href="#" class="mr-3"><span class="text-black fl-bigmug-line-phone351"></span> <span
-                  class="d-none d-md-inline-block ml-2">+2 102 3923 3922</span></a>
-              <a href="#"><span class="text-black fl-bigmug-line-email64"></span> <span
-                  class="d-none d-md-inline-block ml-2">info@domain.com</span></a>
+              <a href="#" class="mr-3"><?$APPLICATION->IncludeComponent(
+                      "bitrix:main.include",
+                      "",
+                      Array(
+                          "AREA_FILE_SHOW" => "page",
+                          "AREA_FILE_SUFFIX" => "phone_number",
+                          "EDIT_TEMPLATE" => ""
+                      )
+                  );?></a>
+              <a href="#"><?$APPLICATION->IncludeComponent(
+                      "bitrix:main.include",
+                      "",
+                      Array(
+                          "AREA_FILE_SHOW" => "page",
+                          "AREA_FILE_SUFFIX" => "email",
+                          "EDIT_TEMPLATE" => ""
+                      )
+                  );?></a>
             </p>
           </div>
           <div class="col-6 col-md-6 text-right">
-            <a href="#" class="mr-3"><span class="text-black icon-facebook"></span></a>
-            <a href="#" class="mr-3"><span class="text-black icon-twitter"></span></a>
-            <a href="#" class="mr-0"><span class="text-black icon-linkedin"></span></a>
+              <a href="#"><?$APPLICATION->IncludeComponent(
+                      "bitrix:main.include",
+                      "",
+                      Array(
+                          "AREA_FILE_SHOW" => "page",
+                          "AREA_FILE_SUFFIX" => "links",
+                          "EDIT_TEMPLATE" => ""
+                      )
+                  );?></a>
           </div>
         </div>
       </div>
@@ -88,11 +116,34 @@ IncludeTemplateLangFile(__FILE__);
       <div class="container py-1">
         <div class="row align-items-center">
           <div class="col-8 col-md-8 col-lg-4">
-            <h1 class=""><a href="index.html" class="h5 text-uppercase text-black"><strong>HomeSpace<span
-                    class="text-danger">.</span></strong></a></h1>
+            <h1 class=""><?$APPLICATION->IncludeComponent(
+                    "bitrix:main.include",
+                    "",
+                    Array(
+                        "AREA_FILE_SHOW" => "page",
+                        "AREA_FILE_SUFFIX" => "home",
+                        "EDIT_TEMPLATE" => ""
+                    )
+                );?></h1>
           </div>
           <div class="col-4 col-md-4 col-lg-8">
             <nav class="site-navigation text-right text-md-right" role="navigation">
+                <?$APPLICATION->IncludeComponent(
+                    "bitrix:menu",
+                    "",
+                    Array(
+                        "ALLOW_MULTI_SELECT" => "N",
+                        "CHILD_MENU_TYPE" => "left",
+                        "DELAY" => "N",
+                        "MAX_LEVEL" => "1",
+                        "MENU_CACHE_GET_VARS" => array(""),
+                        "MENU_CACHE_TIME" => "3600",
+                        "MENU_CACHE_TYPE" => "N",
+                        "MENU_CACHE_USE_GROUPS" => "Y",
+                        "ROOT_MENU_TYPE" => "top",
+                        "USE_EXT" => "N"
+                    )
+                );?>
 
               <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#"
                   class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
