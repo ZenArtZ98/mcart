37d652087bbdd992d1d710fad4fdc4c5f7ce7d82
ZenArtZ98 <sergey.volkov98@yandex.ru>
2024-01-14 12:38:47 +0300

    task 156521 v2

--- local/templates/home/footer.php
+++ local/templates/home/footer.php
@@ -21,31 +21,7 @@ if(! defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
 
         </div>
-        <div class="col-lg-4 mb-5 mb-lg-0">
-          <div class="row mb-5">
-            <div class="col-md-12">
-              <h3 class="footer-heading mb-4">Navigations</h3>
-            </div>
-            <div class="col-md-6 col-lg-6">
-              <ul class="list-unstyled">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Buy</a></li>
-                <li><a href="#">Rent</a></li>
-                <li><a href="#">Properties</a></li>
-              </ul>
-            </div>
-            <div class="col-md-6 col-lg-6">
-              <ul class="list-unstyled">
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Privacy Policy</a></li>
-                <li><a href="#">Contact Us</a></li>
-                <li><a href="#">Terms</a></li>
-              </ul>
-            </div>
-          </div>
 
-
-        </div>
           <?$APPLICATION->IncludeComponent(
 	"bitrix:menu", 
 	"template2", 
@@ -56,7 +32,7 @@ if(! defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 		"MAX_LEVEL" => "1",
 		"MENU_CACHE_GET_VARS" => array(
 		),
-		"MENU_CACHE_TIME" => "3600",
+		"MENU_CACHE_TIME" => "604800",
 		"MENU_CACHE_TYPE" => "A",
 		"MENU_CACHE_USE_GROUPS" => "Y",
 		"ROOT_MENU_TYPE" => "bottom",
