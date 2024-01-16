c8ff57777ef31b1d40b48e7499d8dce88785b5f8
ZenArtZ98 <sergey.volkov98@yandex.ru>
2024-01-16 17:36:16 +0300

    task 156526

--- about_the_service/сontacts/index.php
+++ about_the_service/сontacts/index.php
@@ -2,12 +2,31 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Контакты");
 ?><?$APPLICATION->IncludeComponent(
+	"bitrix:main.feedback",
+	"template1",
+	Array(
+		"EMAIL_TO" => "sergey.volkov98@yandex.ru",
+		"EVENT_MESSAGE_ID" => "",
+		"OK_TEXT" => "Спасибо, ваше сообщение принято.",
+		"REQUIRED_FIELDS" => "",
+		"USE_CAPTCHA" => "Y"
+	)
+);?>
+    <div class="col-lg-4">
+
+<?$APPLICATION->IncludeComponent(
 	"bitrix:main.include",
-	"",
+	".default",
 	Array(
 		"AREA_FILE_SHOW" => "page",
 		"AREA_FILE_SUFFIX" => "inc",
+		"COMPONENT_TEMPLATE" => ".default",
 		"EDIT_TEMPLATE" => "standard.php",
 		"PATH" => ""
 	)
-);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+);?>
+	</div>
+	</div>
+	</div>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
